449de7420056c2583e92bae589aeb5e014f23228
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2023-03-26 18:10:36 +0600

    Добавьте файлы проекта.

---
+++ ITEvents/Pages/Autho.xaml.cs
@@ -0,0 +1,179 @@
+﻿using ITEvents.Entities;
+using ITEvents.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ITEvents.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для Autho.xaml
+	/// </summary>
+	public partial class Autho : Page
+	{
+
+		private int _countUnsuccessful = 0;
+		private Members members = null;
+		private Moderators moderator = null;
+		private Organizer organizer = null;
+		private Jury jury = null;
+
+		public Autho()
+		{
+			InitializeComponent();
+		}
+
+		private void btnEnterGuest_Click(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new Events());
+        }
+
+		private void btnEnter_Click(object sender, RoutedEventArgs e)
+		{
+			string login = tbLogin.Text;
+			string password = pbPassword.Password;
+			var dbConn = EventEntitiesIT.GetContext();
+
+			if(login.Length > 0 && password.Length > 0 )
+			{
+				members = dbConn.Members.Where(x => x.Email == login && x.Password == password).FirstOrDefault();
+				moderator = dbConn.Moderators.Where(x => x.Email == login && x.Password == password).FirstOrDefault();
+				organizer = dbConn.Organizer.Where(x => x.Email == login && x.Password == password).FirstOrDefault();
+				jury = dbConn.Jury.Where(x => x.Email == login && x.Password == password).FirstOrDefault();
+				if(_countUnsuccessful < 3)
+				{
+					if(members != null)
+					{
+						clearForm();
+						MembersWin membersWin = new MembersWin();
+						membersWin.ShowDialog();
+					}
+					else if(moderator != null)
+					{
+						clearForm();
+						ModeratorWin moderatorWin = new ModeratorWin();
+						moderatorWin.ShowDialog();
+					}
+					else if(organizer != null)
+					{
+						clearForm();
+						OrganizerWin organizerWin = new OrganizerWin();
+						organizerWin.ShowDialog();
+					}
+					else if(jury != null)
+					{
+						clearForm();
+						JuryWin juryWin = new JuryWin();
+						juryWin.ShowDialog();
+					}
+					else
+					{
+						_countUnsuccessful++;
+						MessageBox.Show("Неверно введены логин или пароль!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Error);
+						if(_countUnsuccessful == 1)
+						{
+							tblCaptcha.Visibility = Visibility.Visible;
+							tbCaptcha.Visibility = Visibility.Visible;
+							tblCaptcha.Text = Model.Captcha.GeneratedCaptcha();
+							tblCaptcha.TextDecorations = TextDecorations.Strikethrough;
+						}
+					}
+				}
+				else
+				{
+					if (members != null && members.Email == login && members.Password == password && tbCaptcha.Text == tblCaptcha.Text)
+					{
+						clearForm();
+						MembersWin membersWin = new MembersWin();
+						membersWin.ShowDialog();
+					}
+					else if (moderator != null && moderator.Email == login && moderator.Password == password && tbCaptcha.Text == tblCaptcha.Text)
+					{
+						clearForm();
+						ModeratorWin moderatorWin = new ModeratorWin();
+						moderatorWin.ShowDialog();
+					}
+					else if (organizer != null && organizer.Email == login && organizer.Password == password && tbCaptcha.Text == tblCaptcha.Text)
+					{
+						clearForm();
+						OrganizerWin organizerWin = new OrganizerWin();
+						organizerWin.ShowDialog();
+					}
+					else if (jury != null && jury.Email == login && jury.Password == password && tbCaptcha.Text == tblCaptcha.Text)
+					{
+						clearForm();
+						JuryWin juryWin = new JuryWin();
+						juryWin.ShowDialog();
+					}
+					else
+					{
+						MessageBox.Show("Введите данные заново через 10 секунд!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+						tblCaptcha.Text = "";
+						tbCaptcha.Clear();
+						tblCaptcha.Visibility = Visibility.Hidden;
+						tbCaptcha.Visibility = Visibility.Hidden;
+						tbLogin.Clear();
+						tbLogin.IsEnabled = false;
+						pbPassword.Clear();
+						pbPassword.IsEnabled = false;
+						btnEnter.IsEnabled = false;
+						tblTimer.Visibility = Visibility.Visible;
+						CountDown(10, TimeSpan.FromSeconds(1), cur => tblTimer.Text =  cur.ToString() + " сек.");
+					}
+				}
+			}
+			else
+			{
+				MessageBox.Show("Не заполнены поля Логин и Пароль!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+
+		private void enableForm()
+		{
+			tbLogin.IsEnabled = true;
+			pbPassword.IsEnabled = true;
+			btnEnter.IsEnabled = true;
+			tblTimer.Visibility = Visibility.Hidden;
+		}
+
+		private void CountDown(int count,TimeSpan interval, Action<int> ts)
+		{
+			var dt = new DispatcherTimer();
+			dt.Interval = interval;
+			dt.Tick += (_, a) =>
+			{
+				if (count-- == 0)
+				{
+					enableForm();
+					dt.Stop();
+				}
+				else
+					ts(count);
+			};
+			ts(count);
+			dt.Start();
+		}
+
+		private void clearForm()
+		{
+			pbPassword.Clear();
+			tbLogin.Clear();
+			tbCaptcha.Clear();
+			tblCaptcha.Visibility = Visibility.Hidden;
+			tbCaptcha.Visibility = Visibility.Hidden;
+			tblTimer.Visibility = Visibility.Hidden;
+		}
+	}
+}
---
+++ ITEvents/Pages/Events.xaml.cs
@@ -0,0 +1,103 @@
+﻿using ITEvents.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITEvents.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для Events.xaml
+	/// </summary>
+	public partial class Events : Page
+	{
+		public Events()
+		{
+			InitializeComponent();
+
+			var events = EventEntitiesIT.GetContext().Events.ToList();
+			lvEvents.ItemsSource = events;
+			DataContext = this;
+
+			tblAllAmount.Text = events.Count().ToString();
+			//cmbFilterEvent.ItemsSource = events;
+
+			UpdateData();
+		}
+
+		public string[] FilterList { get; set; } =
+		{
+			"Без фильтра",
+			"ИТ",
+			"Биг Дата",
+			"Аналитика",
+			"Информационная безопасность",
+			"Дизайн"
+		};
+
+		private void UpdateData()
+		{
+			var result = EventEntitiesIT.GetContext().Events.ToList();
+
+			if(cmbFilterEvent.SelectedIndex == 1)
+			{
+				result = result.Where(p => p.Direction == "ИТ").ToList();
+			}
+			if (cmbFilterEvent.SelectedIndex == 2)
+			{
+				result = result.Where(p => p.Direction == "Биг Дата").ToList();
+			}
+			if (cmbFilterEvent.SelectedIndex == 3)
+			{
+				result = result.Where(p => p.Direction == "Аналитика").ToList();
+			}
+			if (cmbFilterEvent.SelectedIndex == 4)
+			{
+				result = result.Where(p => p.Direction == "Информационная безопасность").ToList();
+			}
+			if (cmbFilterEvent.SelectedIndex == 5)
+			{
+				result = result.Where(p => p.Direction == "Дизайн").ToList();
+			}
+
+			if (dpFilterDate.Text != "")
+			{
+				result = result.Where(p => p.DATE == dpFilterDate.Text).ToList();
+			}
+			lvEvents.ItemsSource = result;
+			tblResultAmount.Text = result.Count().ToString();
+		}
+
+		private void dpFilterDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+		{
+			UpdateData();
+		}
+
+		private void cmbFilterEvent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			UpdateData();
+		}
+
+		private void lvEvents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var selectedItem = lvEvents.SelectedItem;
+
+			var moreEvent = new MoreEvent();
+
+			moreEvent.DataContext = selectedItem;
+
+			NavigationService.Navigate(moreEvent);
+		}
+	}
+}
